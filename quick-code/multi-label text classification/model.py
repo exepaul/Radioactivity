81bdf9cb1989f8a0efaec0749ab64b06015d2201
Ayodhyankit Paul <3x3paul@gmail.com>
2018-05-14 21:32:39 +0530

    Update model.py

--- quick-code/multi-label text classification/model.py
+++ quick-code/multi-label text classification/model.py
@@ -3,16 +3,34 @@ import numpy as np
 from tensorflow.contrib import rnn
 
 
+batch_size=50
+
+# iteration=int(len(dataset)//batch_size)
+
+epoch=40
+
+
 
 class movies_classifier():
 
     def __init__(self):
 
+        tf.reset_default_graph()
+
+
         input_sentences= tf.placeholder(name='input',shape=[None,None],dtype=tf.int32)
 
         labels = tf.placeholder(name='labels',shape=[None,None],dtype=tf.int32)
 
-        self.placeholder={'input':input_sentences,'output':labels}
+        mode = tf.placeholder(tf.int32, (), name='mode')
+
+        self.placeholder={'input':input_sentences,'output':labels,'mode':mode}
+
+        dropout = tf.cond(
+            tf.equal(mode, 0),  # If
+            lambda: 0.5,  # True
+            lambda: 0.  # False
+        )
 
 
         sequence_leng=tf.count_nonzero(input_sentences,axis=-1)  #10x10x50
@@ -25,13 +43,13 @@ class movies_classifier():
 
             fr_cell=rnn.LSTMCell(num_units=250)
 
-            dropout_fr=rnn.DropoutWrapper(cell=fr_cell,output_keep_prob=0.5)
+            dropout_fr=rnn.DropoutWrapper(cell=fr_cell,output_keep_prob=1.-dropout)
 
         with tf.variable_scope('encoder'):
 
             bw_cell=rnn.LSTMCell(num_units=250)
 
-            dropout_bw=rnn.DropoutWrapper(cell=bw_cell,output_keep_prob=0.5)
+            dropout_bw=rnn.DropoutWrapper(cell=bw_cell,output_keep_prob=1.-dropout)
 
         with tf.variable_scope('encoder') as scope:
 
@@ -47,86 +65,47 @@ class movies_classifier():
 
 
 
+        #
 
 
 
 
-        # self.out={'out':model,'each':fs.c,'result':concat_result,'em':  word_lookup}
 
-        # transform_output=tf.transpose(model,[1,0,2])
+
         #
         weights=tf.get_variable(name='weights',shape=[2*250,28],dtype=tf.float32,initializer=tf.random_uniform_initializer(-0.01,0.01))
         # #
         bias=tf.get_variable(name='bias',shape=[28,],dtype=tf.float32,initializer=tf.random_uniform_initializer(-0.01,0.01))
-        # #
-        # #
+
         logits=tf.matmul(concat_result,weights) + bias
-        #
-        # #
+
         ce=tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.cast(labels,tf.float32),logits=logits)
-        #
-        #
-        # self.out={'ce':ce,'logits':logits,'result':concat_result}
 
         loss=tf.reduce_mean(ce)
         # #
+
+        pred=tf.nn.sigmoid(logits)
+
         threshold=0.5
 
-        # logits = tf.matmul(self.attrs, W) + b
-        #
-        # ce = tf.nn.sigmoid_cross_entropy_with_logits(
-        #     labels=tf.cast(self.labels, tf.float32),
-        #     logits=logits
-        # )
-        # loss = tf.reduce_mean(ce)
-        #
-        # prediction = tf.cast(tf.nn.sigmoid(logits) > threshold, tf.int32)
-        #
-        # accuracy = tf.cast(tf.equal(self.labels, prediction), tf.float32)
-        #
-        # self.out = {
-        #     'prediction': prediction,
-        #     'logits': tf.nn.sigmoid(logits),
-        #     'accuracy': tf.reduce_mean(accuracy),
-        #     'loss': loss
-        # }
-        #
-        # self.train_op = tf.train.AdamOptimizer(lr).minimize(loss)
-
-
-        #regularization
 
 
         #
-        prob=tf.nn.sigmoid(logits)
+        last_prediction=tf.cast(pred>threshold,tf.int32)
         #
-        predi=tf.cast(prob>threshold,tf.int32)
-        #
-        accuracy=tf.cast(tf.equal(labels,predi),tf.float32)
+        accuracy=tf.cast(tf.equal(labels,last_prediction),tf.float32)
         #
         #
         #
-        self.out={'prediction':predi,'accuracy':tf.reduce_mean(accuracy),'loss':loss,'logits':logits}
+        self.out={'prediction':last_prediction,'accuracy':tf.reduce_mean(accuracy),'loss':loss,'logits':logits}
         #
         self.train_op=tf.train.AdamOptimizer().minimize(loss)
 
 
 
-def checking_model(model):
-
-    with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
-        for i in range(100):
-            out_put,_=sess.run([model.out,model.train_op],feed_dict={model.placeholder['input']:np.random.randint(0,10,[10,10]),model.placeholder['output']:np.random.randint(0,10,[10,28])})
-            print(out_put['prediction'],out_put['accuracy'],out_put['loss'])
 
 
 
-
-if "__main__"== __name__:
-
-    bv=movies_classifier()
-    checking_model(bv)
 
 
 
